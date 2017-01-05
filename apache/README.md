810abee09bfeb247c65f75bb7a5e7ea0226ddb50
Arondight <shell_way@foxmail.com>
2017-01-05 17:54:37 +0800

    v2017-01-05.1

--- apache/README.md
+++ apache/README.md
@@ -1,4 +1,4 @@
-这是一个`httpd` 的文件浏览样式，使用方法请查看[httpd.patch](httpd.patch)。这份Patch 是在Arch Linux 的`/etc` 目录下生成的，其他发行版需要对应修改。
+这是一个`apache` 的文件浏览样式，使用方法请查看[apache.patch](apache.patch)。这份Patch 是在Arch Linux 的`/etc` 目录下生成的，其他发行版需要对应修改。
 
 另外需要将`.web` 目录复制到`DocumentRoot`，例如。
 
---
+++ apache/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# ==============================================================================
+# Install profiles for vsftpd
+# ==============================================================================
+# Create by Arondight <shell_way@foxmail.com>
+# ==============================================================================
+
+SUFFIX=$(date +'%Y%m%d-%H%M%S')
+WORKDIR=$(dirname $(readlink -f $0))
+
+# MAIN:
+{
+  echo -ne "Install profiles for apache ...\t"
+  echo "skip"
+}
+
